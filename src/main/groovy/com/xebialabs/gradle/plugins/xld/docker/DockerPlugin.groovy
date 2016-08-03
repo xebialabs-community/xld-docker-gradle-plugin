9f78835027b151a80af0f4e89977784c3093f372
Joris De Winne <joris.dewinne@gmail.com>
2016-08-03 17:31:18 -0500

    Adding local .gradle

--- src/main/groovy/com/xebialabs/gradle/plugins/xld/docker/DockerPlugin.groovy
+++ src/main/groovy/com/xebialabs/gradle/plugins/xld/docker/DockerPlugin.groovy
@@ -17,8 +17,8 @@ class DockerPlugin implements Plugin<Project> {
         DockerPluginExtension dockerPluginExtension = project.extensions.create("xldDocker", DockerPluginExtension)
 
         project.afterEvaluate {
-            Task compileTask = createDockerTask(project, COMPILE_DOCKER_TASK_NAME, ["run", "-v", project.getRootDir().absolutePath + ":/data", "xebialabs/xld_dev_compile:"+dockerPluginExtension.version])
-            Task runTask = createDockerTask(project, RUN_DOCKER_TASK_NAME, ["run", "-p", "4516:4516", "-v", project.getRootDir().absolutePath + ":/data", "-v", System.getProperty("user.home") + "/xl-licenses:/license", "xebialabs/xld_dev_run:"+dockerPluginExtension.version])
+            Task compileTask = createDockerTask(project, COMPILE_DOCKER_TASK_NAME, ["run", "-v", project.getRootDir().absolutePath + ":/data", "-v", System.getProperty("user.home") + "/.gradle:/.gradle", "xebialabs/xld_dev_compile:" + dockerPluginExtension.version])
+            Task runTask = createDockerTask(project, RUN_DOCKER_TASK_NAME, ["run", "-p", "4516:4516", "-v", project.getRootDir().absolutePath + ":/data", "-v", System.getProperty("user.home") + "/xl-licenses:/license", "-v", System.getProperty("user.home") + "/.gradle:/.gradle", "xebialabs/xld_dev_run:" + dockerPluginExtension.version])
             runTask.dependsOn compileTask
         }
     }
